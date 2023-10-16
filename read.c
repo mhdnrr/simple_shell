f90a4cc467a584eddc0aad6db9991b83c323e11e
mhdnrr <mhdnrr@gmail.com>
2023-10-16 06:28:29 -0700

    read.c

---
+++ read.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/*
+ * Reads a command from standard input and stores it in a buffer
+ * @read_cmd:
+ *
+ * cmd: A pointer to a character pointer that will store the command
+ * size: A pointer to a size_t variable that will store the size of the buffer
+ */
+
+void read_cmd(char **cmd, size_t *size)
+{
+
+	ssize_t n = getline(cmd, size, stdin);
+
+	if (n == -1)
+	{
+		if (feof(stdin))
+		{
+
+			print("\n");
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+
+			print("Error\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	(*cmd)[strlen(*cmd) - 1] = '\0';
+}
