3671b11c8e41ef4a790494e0b7dce9c2ce96fbea
mhdnrr <mhdnrr@gmail.com>
2023-10-16 06:55:57 -0700

    excute.c

---
+++ excute.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/*
+ * Executes the given command using a child process
+ * @execute_cmd:
+ *
+ * cmd: The command to execute
+ */
+
+void execute_cmd(const char *cmd)
+{
+	pid_t pid = fork();
+
+	if (pid < 0)
+	{
+		print("Error\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		execlp(cmd, cmd, NULL);
+		print("Error\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		int status;
+
+		waitpid(pid, &status, 0);
+	}
+}
