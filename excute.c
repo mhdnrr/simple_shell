375065e08968457e24e404f090cf154e6f2ae713
mhdnrr <mhdnrr@gmail.com>
2023-10-19 22:55:17 -0700

    All

--- excute.c
+++ excute.c
@@ -5,25 +5,33 @@
  * @cmd: The command to execute.
  */
 
-void execute_cmd(const char *cmd)
+void execute_cmd(char *cmd)
 {
-	pid_t pid = fork();
+	char *arguments[MAX_ARGUMENTS];
+	int i = 0;
 
-	if (pid < 0)
+	char *token = strtok(cmd, " ");
+	while (token != NULL && i < MAX_ARGUMENTS - 1)
 	{
-		print("Error\n");
-		exit(EXIT_FAILURE);
+		arguments[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	arguments[i] = NULL;
+
+	pid_t pid = fork();
+	if (pid == -1)
+	{
+		print("Fork failed\n");
+		return;
 	}
 	else if (pid == 0)
 	{
-		execlp("/bin/sh", "sh", "-c", cmd, NULL);
-		print("Error\n");
+		execvp(arguments[0], arguments);
+		print("Command not found\n");
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		int status;
-
-		waitpid(pid, &status, 0);
+		wait(NULL);
 	}
 }
--- shell.h
+++ shell.h
@@ -1,6 +1,7 @@
 #ifndef SHELL_H
 #define SHELL_H
-#define MAX_COMMAND_LENGTH 100
+#define MAX_COMMAND_LENGTH 1024
+#define MAX_ARGUMENTS 64
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,6 +14,6 @@
 void print(const char *pnt);
 void start_shell(void);
 void read_cmd(char **cmd, size_t *size);
-void execute_cmd(const char *cmd);
+void execute_cmd(char *cmd);
 
 #endif
